c37faa4aa75896d8ac4dc2f4fb940f2feff3ad0e
negabaro <negabaro@gmail.com>
2018-11-21 17:38:41 +0900

    add bind

--- ReactBlogApp-SignIn/html/main.jsx
+++ ReactBlogApp-SignIn/html/main.jsx
@@ -7,8 +7,8 @@ class Signin extends React.Component {
     constructor(props) {
       super(props);
       this.signIn = this.signIn.bind(this);
-      //this.handleEmailChange = this.handleEmailChange.bind(this);
-      //this.handlePasswordChange = this.handlePasswordChange.bind(this);
+      this.handleEmailChange = this.handleEmailChange.bind(this);
+      this.handlePasswordChange = this.handlePasswordChange.bind(this);
       this.state = {
         email:'',
         password:''
